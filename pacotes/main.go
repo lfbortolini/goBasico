a035850a02b185f0b3c6676da24ae09278adddee
Luiz Fernando Bortolini <luiz.bortolini@softplan.com.br>
2022-11-16 21:52:20 -0300

    6. Variáveis

--- pacotes/main.go
+++ pacotes/main.go
@@ -2,14 +2,12 @@ package main
 
 import (
 	"fmt"
-	"modulo/auxiliar"
 
 	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
-	auxiliar.Escrever()
 
 	erro := checkmail.ValidateFormat("lfbortolini@gmail.com")
 	fmt.Println(erro)
