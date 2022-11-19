37db0e53ffb4d2253431054d37c91ede75fced95
Luiz Fernando Bortolini <luiz.bortolini@softplan.com.br>
2022-11-18 21:50:00 -0300

    11. Herança

---
+++ 7 - Herança/heranca.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type pessoa struct {
+	nome      string
+	sobrenome string
+	idade     uint8
+	altura    uint8
+}
+
+type estudante struct {
+	pessoa
+	curso     string
+	faculdade string
+}
+
+func main() {
+	fmt.Println("Herança")
+
+	p1 := pessoa{"João", "Pedro", 20, 178}
+	fmt.Println(p1)
+
+	e1 := estudante{p1, "Engenharia", "USP"}
+	fmt.Println(e1.altura)
+
+	e2 := estudante{pessoa{"Luiz", "Fernando", 10, 20}, "Engenharia", "USP"}
+	fmt.Println(e2)
+
+}
