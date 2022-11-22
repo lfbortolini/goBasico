b0969a856faaa7bfd4c07569d82885a51e142b26
Luiz Fernando Bortolini <luiz.bortolini@softplan.com.br>
2022-11-21 21:27:09 -0300

    13. Arrays e Slices

---
+++ 9 - Arrays e Slices/arrays-e-slices.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	fmt.Println("Arrays e Slices")
+
+	var array1 [5]string
+	array1[0] = "Posição 1"
+	fmt.Println(array1)
+
+	array2 := [5]string{"Posição 1", "Posição 2", "Posição 3", "Posição 4", "Posição 5"}
+	fmt.Println(array2)
+
+	array3 := [...]int{1, 2, 3, 4, 5}
+	fmt.Println(array3)
+
+	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println(slice)
+
+	slice = append(slice, 11)
+	fmt.Println(slice)
+
+	slice2 := array2[1:3]
+	fmt.Println(slice2)
+
+	array2[1] = "Posição alterada"
+	fmt.Println(slice2)
+
+	fmt.Println(reflect.TypeOf(slice))
+}
