3d0bec2ab759331c3abfbd295da19ec7815b2ebe
Luiz Fernando Bortolini <luiz.bortolini@softplan.com.br>
2022-12-17 16:24:05 -0300

    32. Criando a funcionalidade de busca por IP

--- 17 - Aplicação de linha de comando/app/app.go
+++ 17 - Aplicação de linha de comando/app/app.go
@@ -1,6 +1,12 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 // Gerar vai retornar a aplicação de linha de comando pronta para ser executada
 func Gerar() *cli.App {
@@ -8,5 +14,35 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca IPs e Nomes de Servidor na internet"
 
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Busca IPS de enderenço na internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "devbook.com.br",
+				},
+			},
+			Action: buscarIPs,
+		},
+	}
+
 	return app
+}
+
+func buscarIPs(c *cli.Context) {
+	host := c.String("host")
+
+	fmt.Println(host)
+
+	ips, erro := net.LookupIP(host)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
