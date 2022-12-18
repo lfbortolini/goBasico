84a4c945f81994126ef89f4e7a4946f7dbe17c00
Luiz Fernando Bortolini <luiz.bortolini@softplan.com.br>
2022-12-18 11:42:30 -0300

    44. Teste Unitário com Diversos Cenários

--- 19 - Testes automatizados/1 - Introdução/enderecos/enderecos_test.go
+++ 19 - Testes automatizados/1 - Introdução/enderecos/enderecos_test.go
@@ -2,12 +2,34 @@ package enderecos
 
 import "testing"
 
+type cenarioTeste struct {
+	enderecoInserido string
+	retornoEsperado  string
+}
+
 func TestTipoDeEndereco(t *testing.T) {
 	enderecoParaTeste := "Avenida Paulista"
-	tipoEnderecoEsperado := "Rua"
+	tipoEnderecoEsperado := "Avenida"
 	tipoEnderecoRecebido := TipoDeEndereco(enderecoParaTeste)
 
 	if tipoEnderecoRecebido != tipoEnderecoEsperado {
 		t.Errorf("O tipo recebido é diferente do esperado. %s é diferente de %s", tipoEnderecoEsperado, tipoEnderecoRecebido)
+	}
+}
+
+func TestTipoDeEnderecoVariado(t *testing.T) {
+	cenarioTeste := []cenarioTeste{
+		{"Rua ABC", "Rua"},
+		{"Rodovia das Nações", "Rodovia"},
+		{"Praça das Bandeiras", "Tipo Inválido"},
+		{"RUA DAS MULHERES", "Rua"},
+	}
+
+	for _, cenario := range cenarioTeste {
+		tipoEnderecoRecebido := TipoDeEndereco(cenario.enderecoInserido)
+
+		if tipoEnderecoRecebido != cenario.retornoEsperado {
+			t.Errorf("O tipo recebido é diferente do esperado. %s é diferente de %s", cenario.retornoEsperado, tipoEnderecoRecebido)
+		}
 	}
 }
